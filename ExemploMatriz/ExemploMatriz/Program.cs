cf179300a972f5279c616105eea3a47bd3fb961b
isaccaldas <34041465+isaccaldas@users.noreply.github.com>
2024-01-17 22:26:57 -0300

    teste

---
+++ ExemploMatriz/ExemploMatriz/Program.cs
@@ -0,0 +1,42 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+//declarando e iniciando uma matriz com valores default
+string[] matriz = new string[3];
+
+//inserindo valores na matriz
+matriz[0] = "Isac";
+matriz[1] = "Pedro";
+matriz[2] = "Davi";
+
+//lendo valores da matriz
+Console.WriteLine(matriz[0]);
+Console.WriteLine(matriz[1]);
+Console.WriteLine(matriz[2]);
+
+matriz[0] = "Roberto";
+Console.WriteLine($"O Novo valor da primeira posição é {matriz[0]}");
+
+//outra forma de inicializar uma matriz
+string[] matriz2 = {"A", "B"};
+
+//lendo valores da matriz2
+Console.WriteLine(matriz2[0]);
+Console.WriteLine(matriz2[1]);
+Console.WriteLine($"O tamanho da matriz 2 é {matriz2.Length}");
+
+// outra forma de ler uma matriz. Usando foreach
+foreach (var item in matriz)
+{
+    Console.WriteLine(item);
+}
+
+// outro exemplo
+int[] inventory = { 200, 450, 700, 175, 250 };
+int sum = 0;
+foreach (int items in inventory)
+{
+    sum += items;
+}
+
+Console.WriteLine(sum);
---
+++ ExpirationDate/ExpirationDate/Program.cs
@@ -0,0 +1,30 @@
+﻿Random random = new Random();
+int daysUntilExpiration = random.Next(12);
+int discountPercentage = 0;
+
+Console.WriteLine(daysUntilExpiration);
+
+if (daysUntilExpiration > 5 && daysUntilExpiration <= 10)
+{
+
+    Console.WriteLine("Your subscription will expire soon. Renew now!");
+
+}
+else if (daysUntilExpiration >= 2 && daysUntilExpiration <= 5)
+{
+
+    Console.WriteLine($"Your subscription expires in {daysUntilExpiration} days.\r\nRenew now and save 10%!");
+
+}
+
+else if (daysUntilExpiration == 1)
+{
+
+    Console.WriteLine("Your subscription expires within a day!\r\nRenew now and save 20%!");
+
+}
+else {
+
+    Console.WriteLine("Your subscription has expired.");
+
+}
